1944983231554918e08993f21bac201b9a4f7742
kelvoNgash <wamahenya82@gmail.com>
2020-05-21 22:48:32 -0400

    More swagger set ups

--- src/main/java/com/codeflix/swagger/controller/EmployeeController.java
+++ src/main/java/com/codeflix/swagger/controller/EmployeeController.java
@@ -4,6 +4,10 @@ package com.codeflix.swagger.controller;
 import com.codeflix.swagger.exception.ResourceNotFoundException;
 import com.codeflix.swagger.model.Employee;
 import com.codeflix.swagger.repository.EmployeeRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +17,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+
+@Api(value = "Employee Roll-Call System", description = "Operations pertaining to employee in Employee Roll-Call System")
 @RestController
 @RequestMapping("/api/v1")
 public class EmployeeController {
@@ -21,6 +27,14 @@ public class EmployeeController {
     @Autowired
     private EmployeeRepository employeeRepository;
 
+
+    @ApiOperation(value = "Displays a List of available employees", response = List.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully retrieved list"),
+            @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+            @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
+            })
     @GetMapping("/employees")
     public List<Employee> getAllEmployees(){
         return employeeRepository.findAll();
